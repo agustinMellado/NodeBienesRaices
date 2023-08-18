5b5398091d775887a3eaf4203726febb1b590441
Agustin Mellado <aguxmellado@gmail.com>
2023-08-18 17:31:07 -0300

    importacion express, defincion de puerto y ruteo

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+//crear la app
+
+const app = express();
+
+//Routing
+app.get('/', function(req,res) {
+    res.send('hola mundo en express')
+})
+
+//definir el puerto 
+const port =3000;
+app.listen(port, ()=>{
+    console.log(`El servidor esta funcionando en el puerto ${port}`)
+});
