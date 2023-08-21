cbe7e8466880ea66f6de5780515b135639c82896
Agustin Mellado <aguxmellado@gmail.com>
2023-08-21 10:36:47 -0300

    habilitacion de pug

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import usuarioRoutes from './routes/usuarioRoutes.js';
 //crear la app
 
 const app = express();
+//Habilitar Pub templane engine
+app.set('view engine', 'pug')
+app.set('views','./views')
 
 //Routing
 app.use('/', usuarioRoutes) //use para que funcione con todos los metodos
