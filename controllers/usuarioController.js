5b13f4dc7fcf9c24d26c9b67c8745c716a857504
Agustin Mellado <aguxmellado@gmail.com>
2023-08-23 20:25:56 -0300

    creacion de ruta y endpoint

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,9 +1,18 @@
 //zona de controllador
 const formularioLogin= (req, res) => {
     //func. encargada de mostrar una vista.
-    res.render('auth/login')
+    res.render('auth/login',{
+
+    })
+}
+const formularioRegistro= (req, res) => {
+    //func. encargada de mostrar una vista.
+    res.render('auth/registro',{
+        
+    })
 }
 
 export{
-    formularioLogin
+    formularioLogin,
+    formularioRegistro
 }
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
+import { formularioLogin, formularioRegistro } from "../controllers/usuarioController.js";
 
 const router= express.Router();
 //rutas 
-//endpoint
+//endpoint que renderizan vistas
 
 router.get('/login', formularioLogin)
+router.get('/registro',formularioRegistro)
 
 //exportacion
 export default router
