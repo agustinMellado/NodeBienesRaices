6882bce7b63db4474a11551009e96ccfb360a1b7
Agustin Mellado <aguxmellado@gmail.com>
2023-08-25 15:34:10 -0300

    se compartio un objeto con la informacion para la vista, y modificacion en el index para quemuestre el objeto que pasamos

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,13 +1,14 @@
 //zona de controllador
 const formularioLogin= (req, res) => {
-    //func. encargada de mostrar una vista.
+    //.render encargado de mostrar una vista.
     res.render('auth/login',{
 
     })
 }
 const formularioRegistro= (req, res) => {
-    //func. encargada de mostrar una vista.
+    //.render encargado de mostrar una vista.
     res.render('auth/registro',{
+        pagina:'Crear Cuenta'
         
     })
 }
