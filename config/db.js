7d3f28d196dc76d226106143688f99bef1811f03
Agustin Mellado <aguxmellado@gmail.com>
2023-08-25 17:28:46 -0300

    configuracion para la bd

---
+++ config/db.js
@@ -0,0 +1,21 @@
+import Sequelize from 'sequelize';
+
+const db = new Sequelize('bienes_raices_node_mvc', 'root', '',{
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    define:{//agrega la fecha de creacion y de modificacion a la tabla determinada.
+        timestamps: true
+    },
+    //Configura en comportamiento para conexiones nuevas o existentes.
+    pool:{
+        max:5,
+        min:0,
+        acquire:30000,
+        idle:10000
+    },
+    operatorAliases:false,
+})
+export default{
+    db
+}
