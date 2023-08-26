2cc81aaefe835c7973cc08776c25555d73e47e7f
Agustin Mellado <aguxmellado@gmail.com>
2023-08-26 17:20:23 -0300

    se creo el objeto usuario, con sus respectivas variables

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,8 +1,24 @@
 import { DataType } from "sequelize";
 import db from '../config/db.js'
 
-const Usuario = db.define('Usuario',{
+const Usuario = db.define('usuario',{
+    nombre:{
+        type: DataType.STRING,
+        allowNull: false// indica que ese campo no puede ir vacio.
+
+    },
+    email:{
+        type: DataType.STRING,
+        allowNull: false// indica que ese campo no puede ir vacio.
+    },
+    password:{
+        type: DataType.STRING,
+        allowNull: false// indica que ese campo no puede ir vacio.
+    },
+    token:{
+        type: DataType.STRING,
+        confirmado: DataType.Boolean,
+    }
 
 
-    
 })
