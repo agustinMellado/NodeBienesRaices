977199632cc06a23e40c55392b887c188fb7a567
Agustin Mellado <aguxmellado@gmail.com>
2023-08-28 16:00:50 -0300

    correcciones

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,5 +1,6 @@
 import { DataTypes } from "sequelize";
 import db from '../config/db.js'
+import bcrypt from "bcrypt";
 
 const Usuario = db.define('usuario',{
     nombre:{
@@ -18,16 +19,17 @@ const Usuario = db.define('usuario',{
     token:{
         type: DataTypes.STRING, //TIPO DE DATO
         confirmado: DataTypes.BOOLEAN,
-    },
-    hooks:{ 
-        //Antes que se cree el nuevo registro de usuario,interseptamos el password y lo encriptamos.
-        beforeCreate: async function(usuario){
-            const salt = await bcrypt.getSalt(10);//cadena aleatoria que se utiliza como entrada adicional al algoritmo de hash
-            usuario.password= await bcrypt.hash(usuario.password,salt);//asignamos el valor cifrado previo.
+    }},{
+        hooks:{ 
+            //Antes que se cree el nuevo registro de usuario,interseptamos el password y lo encriptamos.
+            beforeCreate: async function(usuario){
+                const salt = await bcrypt.getSalt(10);//cadena aleatoria que se utiliza como entrada adicional al algoritmo de hash
+                usuario.password= await bcrypt.hash(usuario.password,salt);//asignamos el valor cifrado previo.
+            }
+    
         }
-
     }
     
-
-})
+    
+    )
 export default Usuario
