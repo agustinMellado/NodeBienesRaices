d61b3810691ed6587c7cd43786c50588849f95f0
Agustin Mellado <aguxmellado@gmail.com>
2023-08-29 17:29:50 -0300

    hay errores xd

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -80,19 +80,19 @@ const registrar= async (req, res) => {
 
 //funcion que comprueba una cuenta
 const confirmar= async (res,req) => {
-    const {token} = req.params;
+    const { token } = req.params;
+    console.log(token)
+    // //verificar si el token es valido.
+    // const usuario = await Usuario.findOne({ where: { token } })
+    // //si no existe el usuario
+    // if (!usuario) {
+    //     return res.render('auth/confirmar-cuenta', {
+    //         pagina: 'Error al confirmar tu cuenta',
+    //         mensaje: 'Hubo un error al confirmar tu cuenta, intenta de nuevo.',
+    //         error: true
+    //     })
 
-    //verificar si el token es valido
-    const usuario= await Usuario.findOne({where:{token}})
-
-    if (!usuario) {
-        return res.render('auth/confirmar-cuenta', {
-            pagina: 'Error al confirmar tu cuenta',
-            mensaje: 'Hubo un error al confirmar tu cuenta, intenta de nuevo.',
-            error: true
-        })
-
-    }
+    // }
     //confirmar la cuenta
 
 }
