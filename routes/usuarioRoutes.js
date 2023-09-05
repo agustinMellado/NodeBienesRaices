47ee46ae0362511e366a4ef0065d47d64b1f8c07
Agustin Mellado <aguxmellado@gmail.com>
2023-09-05 16:21:19 -0300

    creacion de metodo post para redirijir y tomar esa informacion

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,16 +1,19 @@
 import express from "express";
-import { formularioLogin, formularioRegistro, registrar,confirmar, formularioRecuperarPassword} from "../controllers/usuarioController.js";
+import { formularioLogin, formularioRegistro, registrar,confirmar, formularioRecuperarPassword, resetearPass} from "../controllers/usuarioController.js";
 
 const router= express.Router();
 //rutas 
 //endpoint que renderizan vistas
 
 router.get('/login', formularioLogin)
+
 router.get('/registro',formularioRegistro)
 router.post('/registro',registrar)
 //routing dinamico
 router.get('/confirmar/:token',confirmar)//lee el token
+
 router.get('/recuperar-pass',formularioRecuperarPassword)
+router.post('/recuperar-pass',formularioRecuperarPassword)
 
 //exportacion
 export default router
