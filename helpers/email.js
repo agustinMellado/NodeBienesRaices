7b9f7ecd49d3b66696cd306ea1575d5e51b493b4
Agustin Mellado <aguxmellado@gmail.com>
2023-09-05 17:13:20 -0300

    se realizo funcion de helper para enviar el email de recuperacion de password

--- helpers/email.js
+++ helpers/email.js
@@ -29,7 +29,35 @@ const emailRegistro = async (datos) => {
     });
 
 }
+const emailRecuperarPass= async (datos) => {
 
+    const transport = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+    //tomo datos de forma individual
+    const {email,nombre,token}=datos
+    //Enviar el Email.
+    await transport.sendMail({//formato de email
+        from: 'BienesRaices.com',
+        to: email,
+        subject: 'Reestablece tu cuenta en BienesRaices.com ',//asunto
+        text: 'Reestablece tu cuenta en BienesRaices.com',
+        html:` 
+        <p> Hola ${nombre}, has solicitado reestablecer tu contraseña en BienesRaices.com</p>
+
+        <p>Sigue el siguiente enlace para generar una contraseña nueva:
+        <a href="${process.env.BACKEND_URL}:${process.env.PORT ?? 3000}/auth/confirmar/${token}">Confirmar Cuenta</a></p>
+        <p>Si no creaste esta cuenta, ignorar este mensaje.<p>`
+
+    });
+
+}
 export {
     emailRegistro,
+    emailRecuperarPass
 }
