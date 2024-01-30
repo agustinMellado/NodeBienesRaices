61e0391f7221b12dd70aed13ba411420ee0a1da2
Agustin Mellado <aguxmellado@gmail.com>
2024-01-30 15:21:55 -0300

    se obtiene la informacion de la calle al soltar el pin

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,28 +1,39 @@
-(function() {
+(function () {
 
-    const lat =  -41.13348;
+    const lat = -41.13348;
     const lng = -71.31015;
-    const mapa = L.map('mapa').setView([lat, lng ], 13);
-    let marker;
-    
+    const mapa = L.map('mapa').setView([lat, lng], 13);
+    let marker; //marcador.
+
+    //Utilizar los servicios de Provider y Geocoder.
+    const geocodeService = L.esri.Geocoding.geocodeService();
+
+
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
 
     //el pin
-    marker=new L.marker([lat,lng], {
+    marker = new L.marker([lat, lng], {
         draggable: true,
-        autoPan:true//permite mover el pin y sigue el movimieento del mapa
+        autoPan: true//permite mover el pin y sigue el movimieento del mapa
 
     })
-    .addTo(mapa);
+        .addTo(mapa);
     //detecta el movimiento del pin 
-    marker.on('moveend', function(event) {
-        marker=event.target //elemento que movemos.
+    marker.on('moveend', function (event) {
+        marker = event.target //elemento que movemos.
 
         const posicion = marker.getLatLng();//toma la lat y long del elemento
         //centramos a la posicion seleccionada
         mapa.panTo(new L.LatLng(posicion.lat, posicion.lng))
+
+        //obtener la informacion de las calles al soltar el pin
+        geocodeService.reverse().latlng(posicion, 13).run(function (error, resultado) { 
+            //al presionar larga un popup con la direccion
+            marker.bindPopup(resultado.address.LongLabel) 
+        })
+
     })
 })()
