563bc041e79090f87f72db868303a7eec8cb351e
Agustin Mellado <aguxmellado@gmail.com>
2024-01-30 09:28:34 -0300

    modificacion para correr 2 comandos a la vez

--- public/js/mapa.js
+++ public/js/mapa.js
@@ -16,7 +16,7 @@
   \************************/
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n\n\n//# sourceURL=webpack://bienesraices/./src/js/mapa.js?");
+eval("__webpack_require__.r(__webpack_exports__);\nalert(\"ola soy el mapa\")\n\n//# sourceURL=webpack://bienesraices/./src/js/mapa.js?");
 
 /***/ })
 
