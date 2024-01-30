b0ecc4a4ebb969eb050b2c0fc928c16b9ee8f52e
Agustin Mellado <aguxmellado@gmail.com>
2024-01-30 10:46:25 -0300

    implementacion de mapa con sus styles y scripts

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,1 +1,13 @@
-alert("ola soy el mapa")
+(function() {
+
+    const lat =  -41.13348;
+    const lng = -71.31015;
+    const mapa = L.map('mapa').setView([lat, lng ], 13);
+    
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+
+})()
