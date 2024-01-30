c333b2dbf84fb01c3175dd069c1bdff842fd4a7a
Agustin Mellado <aguxmellado@gmail.com>
2024-01-30 11:51:10 -0300

    obtener la latitud y longitud del pin

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -13,8 +13,16 @@
     //el pin
     marker=new L.marker([lat,lng], {
         draggable: true,
-        autoPan:true//permite mover el pin
+        autoPan:true//permite mover el pin y sigue el movimieento del mapa
 
     })
     .addTo(mapa);
+    //detecta el movimiento del pin 
+    marker.on('moveend', function(event) {
+        marker=event.target //elemento que movemos.
+
+        const posicion = marker.getLatLng();//toma la lat y long del elemento
+        //centramos a la posicion seleccionada
+        mapa.panTo(new L.LatLng(posicion.lat, posicion.lng))
+    })
 })()
